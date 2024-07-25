461a227b3af35e508ff9bb0af10810e164791a17
Chuying Huo <chuo8548@gmail.com>
2024-07-25 11:54:29 -0400

    Initial Commit

---
+++ projectme/projectmeApp.swift
@@ -0,0 +1,17 @@
+//
+//  projectmeApp.swift
+//  projectme
+//
+//  Created by Chuying Huo on 2024-07-25.
+//
+
+import SwiftUI
+
+@main
+struct projectmeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
